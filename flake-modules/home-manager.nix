6462680960ba722fc0589745bc9b153013e6e214
goodrod <david.register@pm.me>
2026-03-14 09:45:19 +0100

    Strip to pure module library: remove hosts and homeConfigurations

--- flake-modules/home-manager.nix
+++ flake-modules/home-manager.nix
@@ -1,58 +1,6 @@
 {
   inputs,
   ...
-}: let
-  mkPkgs = system: import inputs.nixpkgs {inherit system;};
-
-  hyprlandProfile = {...}: {
-    module = {
-      alacritty.enable = true;
-      fuzzel.enable = true;
-      wofi.enable = true;
-      dunst.enable = true;
-      default-home-dirs.enable = true;
-      navi.enable = true;
-      waybar.enable = true;
-      hyprland.enable = true;
-      obsidian.enable = false;
-    };
-  };
-
-  mkHomeConfig = {username, hostProfile ? null, system ? "x86_64-linux"}:
-    inputs.home-manager.lib.homeManagerConfiguration {
-      pkgs = mkPkgs system;
-      extraSpecialArgs = {inherit inputs;};
-      modules =
-        [
-          inputs.self.homeModules.default
-          hyprlandProfile
-          inputs.nixvim.homeModules.nixvim
-          {
-            home.username = username;
-            home.homeDirectory = "/home/${username}";
-            home.stateVersion = "23.11";
-          }
-        ]
-        ++ (
-          if hostProfile != null
-          then [hostProfile]
-          else []
-        );
-    };
-in {
+}: {
   flake.homeModules.default = import ../modules;
-
-  flake.homeConfigurations = {
-    goodrod = mkHomeConfig {username = "goodrod";};
-    test-user = mkHomeConfig {username = "test-user";};
-
-    "goodrod@work" = mkHomeConfig {
-      username = "goodrod";
-      hostProfile = ../hosts/work/home.nix;
-    };
-    "goodrod@private" = mkHomeConfig {
-      username = "goodrod";
-      hostProfile = ../hosts/private/home.nix;
-    };
-  };
 }
