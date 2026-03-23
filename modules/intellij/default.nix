01a5fa0d5b67b63694bb313745a7c09dca4ab8ab
goodrod <david.register@pm.me>
2026-03-23 21:13:25 +0100

    fix: wrap intellij with WLToolkit flag for wayland

--- modules/intellij/default.nix
+++ modules/intellij/default.nix
@@ -2,12 +2,21 @@
 with lib;
 let
   cfg = config.module.intellij;
+  intellij-wrapped = pkgs.symlinkJoin {
+    name = "intellij-wrapped";
+    paths = [ pkgs.jetbrains.idea ];
+    buildInputs = [ pkgs.makeWrapper ];
+    postBuild = ''
+      wrapProgram $out/bin/idea \
+        --add-flags "-Dawt.toolkit.name=WLToolkit"
+    '';
+  };
 in {
   options.module.intellij = {
     enable = mkEnableOption "intellij";
   };
 
   config = mkIf cfg.enable {
-    home.packages = [ pkgs.jetbrains.idea ];
+    home.packages = [ intellij-wrapped ];
   };
 }
