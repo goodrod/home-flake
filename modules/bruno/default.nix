c585bb03c84a0076091c4ab12be7571a52e24cdb
goodrod <david.register@pm.me>
2026-03-23 20:30:32 +0100

    fix: wrap bruno with --no-sandbox for non-NixOS systems

--- modules/bruno/default.nix
+++ modules/bruno/default.nix
@@ -2,12 +2,20 @@
 with lib;
 let
   cfg = config.module.bruno;
+  bruno-wrapped = pkgs.symlinkJoin {
+    name = "bruno-wrapped";
+    paths = [ pkgs.bruno ];
+    buildInputs = [ pkgs.makeWrapper ];
+    postBuild = ''
+      wrapProgram $out/bin/bruno --add-flags "--no-sandbox"
+    '';
+  };
 in {
   options.module.bruno = {
     enable = mkEnableOption "bruno";
   };
 
   config = mkIf cfg.enable {
-    home.packages = [ pkgs.bruno ];
+    home.packages = [ bruno-wrapped ];
   };
 }
